02ff2f9980a8c55b2461b9995d863149dc918f8d
Mocheng <18830279823@163.com>
2023-01-10 17:31:09 +0800

    新增多个属性演示

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -2,15 +2,58 @@
   <div class="container">
     <!-- 操作区域 -->
     <div class="operate-area">
+      <!-- item是否设置高度 -->
       <div class="switch-box">
-        <label for="test">{{ wrapVal }}:&nbsp;</label>
-        <input type="checkbox" id="test" v-model="isWrap" />
+        <label for="isheight">设置项目高度:&nbsp;</label>
+        <input type="checkbox" id="isheight" v-model="isHeight" />
       </div>
+      <!-- flex-wrap -->
+      <div class="switch-box">
+        <label for="iswrap">{{ wrapVal }}:&nbsp;</label>
+        <input type="checkbox" id="iswrap" v-model="isWrap" />
+      </div>
+      <!-- flex-direction -->
+      <div class="select-box">
+        <span>flex-direction:&nbsp;</span>
+        <select v-model="flexDirectionVal">
+          <option
+            v-for="item in options2"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></option>
+        </select>
+      </div>
+      <!-- justify-content -->
       <div class="select-box">
         <span>justify-content:&nbsp;</span>
         <select v-model="justifyContentVal">
           <option
             v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></option>
+        </select>
+      </div>
+      <!-- align-content -->
+      <div class="select-box">
+        <span>align-content:&nbsp;</span>
+        <select v-model="alignContentVal">
+          <option
+            v-for="item in options3"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></option>
+        </select>
+      </div>
+      <!-- align-items -->
+      <div class="select-box">
+        <span>align-items:&nbsp;</span>
+        <select v-model="alignItemsVal">
+          <option
+            v-for="item in options4"
             :key="item.value"
             :label="item.label"
             :value="item.value"
@@ -24,17 +67,33 @@
       :style="{
         width: isWrap ? '55%' : '55%',
         flexWrap: wrapVal,
-        flexDirection: 'row',
+        flexDirection: flexDirectionVal,
         justifyContent: justifyContentVal,
+        alignContent: alignContentVal,
+        alignItems: alignItemsVal,
       }"
     >
       <div
         class="item"
-        :style="{ backgroundColor: item }"
+        :style="{
+          backgroundColor: item,
+          height: isHeight ? '120px' : '',
+          width: '120px',
+        }"
         v-for="(item, index) in colors"
         :key="item"
       >
         {{ index + 1 }}
+      </div>
+      <div
+        class="item"
+        :style="{
+          height: isHeight ? '135px' : '',
+          width: '135px',
+          backgroundColor: 'aqua',
+        }"
+      >
+        {{ colors.length + 1 }}
       </div>
     </div>
   </div>
@@ -43,9 +102,13 @@
 <script setup lang="ts">
 const justifyContentVal = ref("")
 const isWrap = ref(false)
+const isHeight = ref(true)
 const wrapVal = computed(() => {
   return isWrap.value ? "wrap" : "nowrap"
 })
+const flexDirectionVal = ref("")
+const alignContentVal = ref("")
+const alignItemsVal = ref("")
 
 const colors = [
   "#e9f0fd",
@@ -108,6 +171,123 @@ const options = [
     label: "unset",
   },
 ]
+
+const options2 = [
+  {
+    value: "row",
+    label: "row",
+  },
+  {
+    value: "row-reverse",
+    label: "row-reverse",
+  },
+  {
+    value: "column",
+    label: "column",
+  },
+  {
+    value: "column-reverse",
+    label: "column-reverse",
+  },
+]
+
+const options3 = [
+  {
+    value: "flex-start",
+    label: "flex-start",
+  },
+  {
+    value: "center",
+    label: "center",
+  },
+  {
+    value: "flex-end",
+    label: "flex-end",
+  },
+  {
+    value: "start",
+    label: "start",
+  },
+  {
+    value: "end",
+    label: "end",
+  },
+  {
+    value: "space-around",
+    label: "space-around",
+  },
+  {
+    value: "space-between",
+    label: "space-between",
+  },
+  {
+    value: "space-evenly",
+    label: "space-evenly",
+  },
+  {
+    value: "stretch",
+    label: "stretch",
+  },
+  {
+    value: "inherit",
+    label: "inherit",
+  },
+  {
+    value: "initial",
+    label: "initial",
+  },
+  {
+    value: "revert",
+    label: "revert",
+  },
+  {
+    value: "unset",
+    label: "unset",
+  },
+]
+
+const options4 = [
+  {
+    value: "flex-start",
+    label: "flex-start",
+  },
+  {
+    value: "center",
+    label: "center",
+  },
+  {
+    value: "flex-end",
+    label: "flex-end",
+  },
+  {
+    value: "start",
+    label: "start",
+  },
+  {
+    value: "end",
+    label: "end",
+  },
+  {
+    value: "baseline",
+    label: "baseline",
+  },
+  {
+    value: "stretch",
+    label: "stretch",
+  },
+  {
+    value: "inherit",
+    label: "inherit",
+  },
+  {
+    value: "initial",
+    label: "initial",
+  },
+  {
+    value: "unset",
+    label: "unset",
+  },
+]
 </script>
 
 <style scoped lang="scss">
@@ -131,7 +311,6 @@ const options = [
       align-items: center;
       label {
         display: inline-block;
-        width: 90px;
         text-align: end;
       }
       input {
@@ -140,9 +319,9 @@ const options = [
         height: 20px;
       }
     }
-    .select-box input {
-      width: 155px;
-      height: 30px;
+
+    .select-box select {
+      min-width: 150px;
     }
   }
 
@@ -154,17 +333,16 @@ const options = [
     border: 3px solid #7b857f;
     border-radius: 10px;
     display: flex;
-
     .item {
-      width: 120px;
-      height: 120px;
       border-radius: 8px;
       border: 2px solid #2c38e0;
       color: #000;
       font-weight: bolder;
       font-size: 20px;
-      line-height: 120px;
+      display: flex;
       text-align: center;
+      justify-content: center;
+      align-items: center;
     }
   }
 }
